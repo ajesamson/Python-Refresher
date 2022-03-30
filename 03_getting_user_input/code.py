62535808f0c967221838e428c434251975c5fd74
Samson Alajede <salajede@microsoft.com>
2022-03-31 00:39:53 +0100

    getting user input

---
+++ 03_getting_user_input/code.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+print(name)
+
+age = input("How old are you: ")
+age = int(age);
+height = input("How tall are you")
+height = float(height)
+
+print(f"Hey, you are {age} years old and {height:.2f}ft tall");
