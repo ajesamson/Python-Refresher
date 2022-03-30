e9e9fd4e78b40ff0ce45e8a82aa41532b6d05c89
Samson Alajede <salajede@microsoft.com>
2022-03-31 00:39:19 +0100

    string formatting

---
+++ 02_string_formatting/code.py
@@ -0,0 +1,12 @@
+num1 = 4;
+num2 = 4;
+result = num1 * num2;
+
+first_name = "Samson";
+last_name = "Alajede";
+
+greeting = "Hello, {} {}";
+with_name = greeting.format(first_name, last_name);
+
+print(f"{num1} * {num2}, {result}");
+print(with_name);
