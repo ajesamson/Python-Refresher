67dae25d7ac038db12612d41c44c2ea8638350e6
ajesamson <ajesamson@users.noreply.github.com>
2022-06-25 16:30:09 +0100

    Lists, tuples and sets

---
+++ 05_lists_tuples_sets/code.py
@@ -0,0 +1,15 @@
+# List - you can add or remove an item
+# Printing is orderly by index - l[0]
+# Add items with append - l.append("Toad")
+l = ["Bob", "Rolf", "Anne"]
+
+# tuple - you cannot add or remove item
+# printing is orderly by index t[0]
+t = ("Bob", "Rolf", "Anne")
+
+# sets - you can add or remove items
+# Printing is not orderly
+# No subscript notation - s[2]
+# Add items is Add - s.add("Toad")
+# Empty set as set() - s = set()
+s = {"Bob", "Rolf", "Anne"}
