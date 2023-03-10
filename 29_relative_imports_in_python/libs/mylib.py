596954ad98f1013294e9617a047179cfb698e5b4
ajesamson <ajesamson@users.noreply.github.com>
2023-03-10 10:32:08 +0100

    Relative imports in python

---
+++ 29_relative_imports_in_python/libs/mylib.py
@@ -0,0 +1,5 @@
+from libs.operations import operator
+# from .operations import operator # relative import
+# this relative import will not work if you run the file directly
+
+print("mylib.py:", __name__)
