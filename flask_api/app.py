e989677875025ca42c7b493240bad194ef1be41f
ajesamson <ajesamson@users.noreply.github.com>
2023-03-16 10:03:46 +0100

    Added store post, get and post iem endpoints

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,7 +1,41 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+stores = [{"name": "Lagos Store", "items": [{"name": "My Item", "price": 15.99}]}]
+
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    data = request.get_json()
+    new_store = {"name": data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+
+def find_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return None
+
+
+@app.post("/store/<string:name>/item")
+def create_store_item(name):
+    data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": data["name"], "price": data["price"]}
+            store.items.append(new_item)
+            return new_item, 201
+    return {"error": "Invalid store name provided"}
