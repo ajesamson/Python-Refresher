576368f999a2439d993542905fcf5fc8505e4a80
ajesamson <ajesamson@users.noreply.github.com>
2023-03-17 09:59:28 +0100

    Added store and store item retrieval endpoints

--- flask_api/app.py
+++ flask_api/app.py
@@ -7,7 +7,7 @@ stores = [{"name": "Lagos Store", "items": [{"name": "My Item", "price": 15.99}]
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return {"message": "Welcome to flask store"}
 
 
 @app.get("/store")
@@ -33,9 +33,25 @@ def find_store(name):
 @app.post("/store/<string:name>/item")
 def create_store_item(name):
     data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": data["name"], "price": data["price"]}
-            store.items.append(new_item)
-            return new_item, 201
-    return {"error": "Invalid store name provided"}
+    store = find_store(name)
+    if store:
+        new_item = {"name": data["name"], "price": data["price"]}
+        store.items.append(new_item)
+        return new_item, 201
+    return {"error": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    store = find_store(name)
+    if store:
+        return store
+    return {"error": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_store_items(name):
+    store = find_store(name)
+    if store:
+        return {"items": store["items"]}
+    return {"error": "Store not found"}, 404
